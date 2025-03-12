9b7eef8dd140b2c69097abbc2d45996e56eab5d9
serignemamesarr <serignemamesarr@esp.sn>
2025-03-12 22:33:17 +0100

    home page and route setup

--- src/app/portal/auth.module.ts
+++ src/app/portal/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './components/connexion/connexion.component'; // Import the ConnexionComponent
 import { RegisterComponent } from './components/register/register.component'; // Import the RegisterComponent
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'login', component: ConnexionComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent }
 ];
 
