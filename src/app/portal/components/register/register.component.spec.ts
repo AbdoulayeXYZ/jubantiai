0325383fa5ccf8bd20f4c67fa0906538b7c9e32a
mdmbest <mamediarram664@gmail.com>
2025-03-13 00:20:42 +0000

    update project

--- src/app/portal/components/register/register.component.spec.ts
+++ src/app/portal/components/register/register.component.spec.ts
@@ -29,7 +29,13 @@ describe('RegisterComponent', () => {
   });
 
   it('should mark form as valid when all fields are filled correctly', () => {
-  
+    component.registerForm.setValue({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@example.com',
+      password: 'password123',
+      role: 'student'
+    });
     
     expect(component.registerForm.valid).toBeTruthy();
   });
