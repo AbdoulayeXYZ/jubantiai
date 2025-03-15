890ce4234a8a5745c01787fc6a89cad96f190966
ahmadoulkhadim <khadimtoure712@gmail.com>
2025-03-15 18:31:32 +0000

    la partie summission

--- src/app/student/student-sidebar/student-sidebar.component.ts
+++ src/app/student/student-sidebar/student-sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-student-sidebar', // Doit correspondre au template
   standalone: false,
